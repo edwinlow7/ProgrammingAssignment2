ba8e0c15c7e9602167d5c06f24eb122568c9acfe
edwinlow7 <edwinlowkf@gmail.com>
2014-09-21 13:08:57 +0800

    Rev2

--- cachematrix.R
+++ cachematrix.R
@@ -4,29 +4,32 @@
 ##second function check if matric is set or changed. if not it calculates
 ##inverse and cache it.
 
-## This function creates a special "matrix" object that can cache its inverse	
+## This function creates a special "matrix" object that can cache its inverse  
 
 makeCacheMatrix <- function(x = matrix()) {
-	#Cache Matrix Value
-	Cache_Matrix<-NULL
-	set <- function(y) {
-      	x <<- y
-            m <<- NULL
-      }
-	
-	#Get Matrix value
-	get <- function()x
-	
-	#Set inverse matrix
-	setInverse<-function(InvMatrix)Cache_Matrix<<-InvMatrix
-	
-	#Get inverse matrix
-	getInverse<-function()Cache_Matrix
-	
-	#create list
-	list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
-	
-
+  
+  #Cache Matrix Value
+  Cache_Matrix<-NULL
+  set <- function(y) {
+    x <<- y
+    Cache_Matrix <<- NULL
+  }
+  
+  #Get Matrix value
+  get <- function()x
+  
+  #Set inverse matrix
+  setInverse<-function(InvMatrix)
+    Cache_Matrix<<-InvMatrix
+  
+  
+  #Get inverse matrix
+  getInverse<-function()Cache_Matrix
+  
+  #create list
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+  
+  
 }
 
 
@@ -36,23 +39,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ##the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-      ##Get cache inverse matrix
-	Inv<-x$getInverse()
-
-	if(!is.null(Inv)) {
-      	message("Getting cached data")
-		
-		## gets the original matrix from the cache matrix. To check if matrix 
-		##was changed or not
-		Original<-solve(Inv)
-		if(identical(Original,x$get()))
-			return(Inv)
-		else
-			message("Original Matrix was changed. Recalculating inverse matrix")
-
-	}
-	data<-x$get()
-	newInv<-solve(data)
-	x$setInverse(newInv)
-	newInv
+  ##Get cache inverse matrix
+  Inv<-x$getInverse()
+  
+  if(!is.null(Inv)) {
+    message("Getting cached data")
+    return(Inv)
+  }
+  data<-x$get()
+  newInv<-solve(data)
+  x$setInverse(newInv)
+  newInv
 }
